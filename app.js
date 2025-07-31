92735592fbf6a60cddd1bc4baaa186f57edf6662
dev_mrahsansaleem <ahsansaleem6389@gmail.com>
2025-07-31 23:05:48 +0500

    update provider

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const multer = require('multer');
-const sgMail = require('@sendgrid/mail');
+// const sgMail = require('@sendgrid/mail');
 const morgan = require('morgan');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+const SibApiV3Sdk = require('sib-api-v3-sdk');
+
+
 
 const dotenv = require('dotenv');
 dotenv.config("./.env")
 const app = express();
 
-
 const port = process.env.PORT || 8080
-
-sgMail.setApiKey(process.env.API_KEY || 'not key found')
-
+// sgMail.setApiKey(process.env.API_KEY || 'not key found')
 // Middleware for file uploads
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
+
+
+const BREVO_API_KEY = process.env.BREVO_API_KEY || 'not key found'
+SibApiV3Sdk.ApiClient.instance.authentications['api-key'].apiKey = BREVO_API_KEY
+
 
 app.use(express.json());
 app.use(morgan('dev'))
@@ -31,34 +35,60 @@ app.get("/", (req, res) => {
 app.post('/send-email', upload.single('file'), async (req, res) => {
     const { to, from, subject, text } = req.body;
     const file = req.file;
-
+    const key = process.env.API_KEY
+    console.log(key)
     // Check if file was uploaded
     if (!to || !from || !subject || !text) {
         return res.status(400).send('Request is not formated correctly contact admin');
     }
     const listOfEmails = to.split(',')
-    var msg = {
-        to: listOfEmails,
-        from,
-        subject,
-        text,
-    };
+    // var msg = {
+    //     to: listOfEmails,
+    //     from,
+    //     subject,
+    //     text,
+    // };
+    // if (file) {
+    //     msg['attachments'] = [
+    //         {
+    //             content: file.buffer.toString('base64'),
+    //             filename: file.originalname,
+    //             type: file.mimetype,
+    //             disposition: 'attachment',
+    //         },
+    //     ]
+    // }
+
+    let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+    let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+
+    sendSmtpEmail.subject = subject;
+    sendSmtpEmail.htmlContent = text;
+    sendSmtpEmail.sender = { email: from };
+    sendSmtpEmail.to = listOfEmails.map(email => ({ email }));
+
     if (file) {
-        msg['attachments'] = [
-            {
-                content: file.buffer.toString('base64'),
-                filename: file.originalname,
-                type: file.mimetype,
-                disposition: 'attachment',
-            },
-        ]
+        sendSmtpEmail.attachment = [{
+            content: file.buffer.toString('base64'),
+            name: file.originalname
+        }];
     }
+
     try {
-        await sgMail.send(msg);
-        res.status(200).send({ msg: 'Email sent successfully!' });
+        const data = await apiInstance.sendTransacEmail(sendSmtpEmail);
+        res.status(200).send({ msg: 'Email sent successfully!', data });
     } catch (error) {
-        res.status(500).send({ msg: 'Error sending email.' });
+        console.error(BREVO_API_KEY, 'Error:', error);
+        res.status(500).send({ msg: 'Error sending email.', error });
     }
+    // try {
+    //     await sgMail.send(msg);
+    //     res.status(200).send({ msg: 'Email sent successfully!' });
+    // } catch (error) {
+    //     res.status(500).send({ msg: 'Error sending email.' });
+    // }
+
+
 });
 
 
